482c2496c815c6831a38b98624713e5e77aea312
Iken Bilal <bilal11iken@gmail.com>
2025-09-20 18:45:23 +0100

    test

--- DART/learning.dart
+++ DART/learning.dart
@@ -1,35 +1,52 @@
 
 
+// void main(){
+//   String name = "Bilal";
+//   int age = 11;
+
+//   print("salm $name 3mrk $age");
+
+// var Y = 11;
+// print(Y);
+// dynamic x = 2;
+// print(x);
+// x = 3;
+
+// print(x);
+
+
+// String nom = "AHMED";
+// int ag = 21;
+// double taille = 188.2;
+// bool men = true ;
+// var city = "beni mellal ";
+
+// print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
+
+// city = "casablanca";
+// print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
+
+// final time = DateTime.now();
+// print(time);
+
+// const pi = 3.14;
+// print(pi);
+
+
+
+// }
+import 'dart:io';
+
+
 void main(){
-  String name = "Bilal";
-  int age = 11;
+  print("ch7al f 3MRK");
 
-  print("salm $name 3mrk $age");
-
-var Y = 11;
-print(Y);
-dynamic x = 2;
-print(x);
-x = 3;
-
-print(x);
-
-
-String nom = "AHMED";
-int ag = 21;
-double taille = 188.2;
-bool men = true ;
-var city = "beni mellal ";
-
-print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
-
-city = "casablanca";
-print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
-
-final time = DateTime.now();
-print(time);
-
-const pi = 3.14;
-print(pi);
-
+  String? input = stdin.readLineSync();
+  int age = int.parse(input!);
+  
+  if(age >= 18 ){
+    print("agee");
+  }else {
+    print("minor");
+  }
 }
