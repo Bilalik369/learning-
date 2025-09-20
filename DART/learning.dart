d6873c77a282aac264ce4244d67b2f4692cff1ea
Iken Bilal <bilal11iken@gmail.com>
2025-09-20 17:53:00 +0100

    test

--- DART/learning.dart
+++ DART/learning.dart
@@ -1,5 +1,18 @@
+
+
 void main(){
-  print("hello word")
+  String name = "Bilal";
+  int age = 11;
+
+  print("salm $name 3mrk $age");
+
+var Y = 11;
+print(Y);
+dynamic x = 2;
+print(x);
+x = 3;
+
+print(x);
+
+
 }
-
-
