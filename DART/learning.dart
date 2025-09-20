db60474d8b8163e8d033a39e42809e3d27dc41fb
Iken Bilal <bilal11iken@gmail.com>
2025-09-20 18:13:11 +0100

    varible

--- DART/learning.dart
+++ DART/learning.dart
@@ -15,4 +15,21 @@ x = 3;
 print(x);
 
 
+String nom = "AHMED";
+int ag = 21;
+double taille = 188.2;
+bool men = true ;
+var city = "beni mellal ";
+
+print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
+
+city = "casablanca";
+print("smiti $nom 3omri $ag Taille dyali $taille rajel $men sakn f $city");
+
+final time = DateTime.now();
+print(time);
+
+const pi = 3.14;
+print(pi);
+
 }
